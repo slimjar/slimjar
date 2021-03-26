984951eef6139520be58c0505b873cd118a1c78e
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-26 14:09:17 +0530

    Added data fields to Dependency and Repository classes

--- downloader/src/main/java/io/github/vshnv/slimjar/data/Dependency.java
+++ downloader/src/main/java/io/github/vshnv/slimjar/data/Dependency.java
@@ -1,4 +1,42 @@
 package io.github.vshnv.slimjar.data;
 
-public class Dependency {
+import java.util.Objects;
+
+public final class Dependency {
+    private final String groupId;
+    private final String artifactId;
+    private final String version;
+
+    public Dependency(String groupId, String artifactId, String version) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dependency that = (Dependency) o;
+        return groupId.equals(that.groupId) &&
+                artifactId.equals(that.artifactId) &&
+                version.equals(that.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupId, artifactId, version);
+    }
 }
--- downloader/src/main/java/io/github/vshnv/slimjar/data/Repository.java
+++ downloader/src/main/java/io/github/vshnv/slimjar/data/Repository.java
@@ -1,4 +1,42 @@
 package io.github.vshnv.slimjar.data;
 
+import java.util.Objects;
+
 public class Repository {
+    private final String id;
+    private final String name;
+    private final String url;
+
+    public Repository(String id, String name, String url) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Repository that = (Repository) o;
+        return id.equals(that.id) &&
+                Objects.equals(name, that.name) &&
+                url.equals(that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, url);
+    }
 }
