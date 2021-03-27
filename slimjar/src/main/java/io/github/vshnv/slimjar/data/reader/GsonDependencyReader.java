71babe2faad0f71cac1cce0b2a29bdda847f3c2d
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-28 04:13:56 +0530

    Refactored Dependency Reader class name

---
+++ slimjar/src/main/java/io/github/vshnv/slimjar/data/reader/GsonDependencyReader.java
@@ -0,0 +1,28 @@
+package io.github.vshnv.slimjar.data.reader;
+
+import com.google.gson.Gson;
+import io.github.vshnv.slimjar.data.DependencyData;
+
+import java.io.*;
+import java.util.function.Function;
+
+public class GsonDependencyReader implements DependencyReader {
+    private final Function<Void, InputStream> inputStreamProducer;
+    private final Gson gson;
+
+    public GsonDependencyReader(Function<Void, InputStream> inputStreamProducer, Gson gson) {
+        this.inputStreamProducer = inputStreamProducer;
+        this.gson = gson;
+    }
+
+    @Override
+    public DependencyData read() {
+        try (InputStream stream = inputStreamProducer.apply(null)) {
+            InputStreamReader inputStreamReader = new InputStreamReader(stream);
+            return gson.fromJson(inputStreamReader, DependencyData.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ slimjar/src/test/java/io/github/vshnv/slimjar/GsonDependencyReaderTest.java
@@ -0,0 +1,27 @@
+package io.github.vshnv.slimjar;
+
+import com.google.gson.Gson;
+import io.github.vshnv.slimjar.data.Dependency;
+import io.github.vshnv.slimjar.data.DependencyData;
+import io.github.vshnv.slimjar.data.reader.DependencyReader;
+import io.github.vshnv.slimjar.data.reader.GsonDependencyReader;
+import junit.framework.TestCase;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class GsonDependencyReaderTest extends TestCase {
+    private final Gson gson = new Gson();
+    public void testLoadDependencies() {
+        DependencyData data = new DependencyData(Arrays.asList(
+                new Dependency("dep-a", "somerepo.tld/path/tojar", Collections.emptyList()),
+                new Dependency("dep-b", "somerepo2.tld/path/tojar", Collections.emptyList()),
+                new Dependency("dep-c", "somerepo3.tld/path/tojar", Collections.emptyList())
+        ));
+        String jsonData = gson.toJson(data);
+        DependencyReader reader = new GsonDependencyReader(v -> new ByteArrayInputStream(jsonData.getBytes()), gson);
+        DependencyData loadedData = reader.read();
+        assertEquals(loadedData, data);
+    }
+}
