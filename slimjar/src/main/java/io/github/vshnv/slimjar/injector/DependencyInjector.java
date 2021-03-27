f40c5ee90412aaad16fc1ca5382c2de162ce251f
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-28 04:37:10 +0530

    Removed unused import

--- slimjar/src/main/java/io/github/vshnv/slimjar/injector/DependencyInjector.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/injector/DependencyInjector.java
@@ -3,7 +3,6 @@ package io.github.vshnv.slimjar.injector;
 import io.github.vshnv.slimjar.data.DependencyData;
 import io.github.vshnv.slimjar.injector.loader.Injectable;
 
-import java.io.IOException;
 
 public interface DependencyInjector {
     void inject(final Injectable injectable, final DependencyData dependencyData) throws InjectionFailedException;
