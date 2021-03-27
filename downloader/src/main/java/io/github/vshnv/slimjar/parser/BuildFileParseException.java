260cd880f0bfc95d3136ca9236c4a548b9f62b06
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-27 15:18:22 +0530

    Removed unnecessary downloader module

--- downloader/src/main/java/io/github/vshnv/slimjar/parser/BuildFileParseException.java
+++
@@ -1,7 +0,0 @@
-package io.github.vshnv.slimjar.parser;
-
-public final class BuildFileParseException extends Exception {
-    public BuildFileParseException(Exception exception) {
-        super("Failed to parse build file!", exception);
-    }
-}
