8e49c9320ddf4b5b0008e0fc43bb0b9628a3857c
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-29 01:58:48 +0530

    Simplified InjectableClassLoader

--- slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/InjectableClassLoader.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/InjectableClassLoader.java
@@ -1,19 +1,58 @@
 package io.github.vshnv.slimjar.injector.loader;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public final class InjectableClassLoader extends URLClassLoader implements Injectable {
-    public InjectableClassLoader(final ClassLoader parent) {
-        super(new URL[0], parent);
-    }
+    private final String entryPointClass;
 
-    public InjectableClassLoader(final URL[] urls, final ClassLoader parent) {
-        super(urls, parent);
+    public InjectableClassLoader(final String entryPointClass) {
+        super(new URL[0]);
+        this.entryPointClass = entryPointClass;
     }
 
     @Override
     public void inject(final URL url) {
         addURL(url);
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        if (name.equals(entryPointClass)) {
+            Class<?> clazz = findClass(name);
+            return clazz;
+        }
+        return super.loadClass(name);
+    }
+
+    @Override
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] b = loadClassFromFile(name);
+        if (b == null) return null;
+        return defineClass(name, b, 0, b.length);
+    }
+
+    private byte[] loadClassFromFile(String fileName)  {
+        InputStream inputStream = getParent().getResourceAsStream(
+                fileName.replace('.', File.separatorChar) + ".class");
+        if (inputStream == null) {
+            return null;
+        }
+        byte[] buffer;
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        int nextValue = 0;
+        try {
+            while ( (nextValue = inputStream.read()) != -1 ) {
+                byteStream.write(nextValue);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        buffer = byteStream.toByteArray();
+        return buffer;
     }
 }
