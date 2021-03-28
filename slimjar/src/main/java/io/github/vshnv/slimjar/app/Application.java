469962ff164e60acea5eef17ef203b2f348ca856
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-29 02:02:49 +0530

    Added Application class for easy facade design

---
+++ slimjar/src/main/java/io/github/vshnv/slimjar/app/Application.java
@@ -0,0 +1,32 @@
+package io.github.vshnv.slimjar.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Application {
+    private final Map<Class<?>, Object> facadeMapping = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    protected final <T, U extends T> T registerFacade(Class<T> clazz, U t) {
+        Object obj = facadeMapping.put(clazz, t);
+        if (obj != null && !(clazz.isAssignableFrom(obj.getClass()))) {
+            throw new IllegalStateException("Previous facade value did not conform to type restriction!");
+        }
+        return (T) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public final <T> T getFacade(Class<T> clazz) {
+        Object obj = facadeMapping.get(clazz);
+        if (obj == null) return null;
+        if (!(clazz.isAssignableFrom(obj.getClass()))) {
+            throw new IllegalStateException("Current facade value does not conform to type restriction!");
+        }
+        return (T) obj;
+    }
+
+
+    public static ApplicationBuilder builder() {
+        return new ApplicationBuilder();
+    }
+}
