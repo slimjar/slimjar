b335eff3c820a829c689d7548e0f25d71fa8ebb7
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-29 01:59:17 +0530

    Added URLClassLoader wrapper

--- slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/Injectable.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/Injectable.java
@@ -1,7 +1,13 @@
 package io.github.vshnv.slimjar.injector.loader;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.net.URLClassLoader;
 
 public interface Injectable {
-    void inject(final URL url);
+    void inject(final URL url) throws InvocationTargetException, IllegalAccessException;
+    
+    static WrappedInjectableClassLoader wrap(URLClassLoader classLoader) {
+        return new WrappedInjectableClassLoader(classLoader);
+    }
 }
