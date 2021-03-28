527f737ef5a31a365c68aea324e8e2b8e35c7816
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-29 02:04:12 +0530

    Added test case for ApplicationBuilder

---
+++ slimjar/src/test/java/io/github/vshnv/slimjar/app/ApplicationBuilderTest.java
@@ -0,0 +1,17 @@
+package io.github.vshnv.slimjar.app;
+
+import io.github.vshnv.slimjar.data.DependencyData;
+import io.github.vshnv.slimjar.injector.loader.InjectableClassLoader;
+import junit.framework.TestCase;
+
+import java.util.Collections;
+
+public class ApplicationBuilderTest extends TestCase {
+    public void testInjectingApplicationLoader() throws ReflectiveOperationException {
+        Application app = AppendingApplication.builder()
+                .withDependencies(new DependencyData(Collections.emptyList()))
+                .buildInjectable("io.github.vshnv.slimjar.app.DummyApplication");
+        assertNotNull(app);
+        assertEquals(app.getClass().getClassLoader().getClass(), InjectableClassLoader.class);
+    }
+}
