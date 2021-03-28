5e3d795e72c8b101d2f3e6efe7728147816c1e64
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-29 01:58:15 +0530

    Added fetching parameter types from object array

---
+++ slimjar/src/main/java/io/github/vshnv/slimjar/util/Parameters.java
@@ -0,0 +1,15 @@
+package io.github.vshnv.slimjar.util;
+
+public final class Parameters {
+    private Parameters() { }
+
+    public static Class<?>[] typesFrom(Object... args) {
+        Class<?>[] result = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+            Object current = args[i];
+            if (current == null) continue;
+            result[i] = current.getClass();
+        }
+        return result;
+    }
+}
