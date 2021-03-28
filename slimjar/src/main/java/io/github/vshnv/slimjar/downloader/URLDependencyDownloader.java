d8f27a1f40c4e25d4beeddee0d180965e48062a2
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-03-28 14:30:01 +0530

    Refactored DependencyDownloader implementation

---
+++ slimjar/src/main/java/io/github/vshnv/slimjar/downloader/URLDependencyDownloader.java
@@ -0,0 +1,47 @@
+package io.github.vshnv.slimjar.downloader;
+
+import io.github.vshnv.slimjar.data.Dependency;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.function.Function;
+
+public final class URLDependencyDownloader implements DependencyDownloader {
+    private final Function<String, OutputWriter> outputWriterProducer;
+
+    public URLDependencyDownloader(final Function<String, OutputWriter> outputWriterProducer) {
+        this.outputWriterProducer = outputWriterProducer;
+    }
+
+    @Override
+    public URL download(final Dependency dependency) throws IOException {
+        final URL url = new URL(dependency.getUrl());
+        final URLConnection connection = createDownloadConnection(url);
+        final String fileName = dependency.getName() + ".jar";
+        final InputStream inputStream = connection.getInputStream();
+        final OutputWriter outputWriter = outputWriterProducer.apply(fileName);
+        outputWriter.writeFrom(inputStream, connection.getContentLength());
+        tryDisconnect(connection);
+        return url;
+    }
+
+    private URLConnection createDownloadConnection(final URL url) throws IOException {
+        final URLConnection connection =  url.openConnection();
+        if (connection instanceof HttpURLConnection) {
+            final HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            final int responseCode = httpConnection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                throw new IOException("Could not download from" + url);
+            }
+        }
+        return connection;
+    }
+    private void tryDisconnect(final URLConnection urlConnection) {
+        if (urlConnection instanceof HttpURLConnection) {
+            ((HttpURLConnection) urlConnection).disconnect();
+        }
+    }
+}
