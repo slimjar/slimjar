b9334dc8aa38c053a090fb824a7057a5ca5dc72d
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-04 03:25:02 +0530

    Dependency path refactors

--- slimjar/src/main/java/io/github/vshnv/slimjar/resolver/strategy/MavenPathResolutionStrategy.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/resolver/strategy/MavenPathResolutionStrategy.java
@@ -4,14 +4,14 @@ import io.github.vshnv.slimjar.resolver.data.Dependency;
 import io.github.vshnv.slimjar.resolver.data.Repository;
 
 public class MavenPathResolutionStrategy implements PathResolutionStrategy {
-    private static final String PATH_FORMAT = "%s/%s/%s/%s/%2$s-%4$s.jar";
+    private static final String PATH_FORMAT = "%s%s/%s/%s/%3$s-%4$s.jar";
     @Override
     public String pathTo(Repository repository, Dependency dependency) {
         return String.format(
                 PATH_FORMAT,
                 repository.getUrl(),
+                dependency.getGroupId().replace('.', '/'),
                 dependency.getArtifactId(),
-                dependency.getGroupId(),
                 dependency.getVersion()
         );
     }
