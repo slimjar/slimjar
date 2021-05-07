4d18f76d4f47e62b46b39a435da2ff74f7aa73be
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-07 15:11:45 +0530

    Added JSON.simple reader

--- slimjar/src/main/java/io/github/vshnv/slimjar/resolver/reader/SimpleJsonDependencyReader.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/resolver/reader/SimpleJsonDependencyReader.java
@@ -1,11 +1,13 @@
 package io.github.vshnv.slimjar.resolver.reader;
 
-import com.google.gson.*;
 import io.github.vshnv.slimjar.relocation.RelocationRule;
 import io.github.vshnv.slimjar.resolver.data.Dependency;
 import io.github.vshnv.slimjar.resolver.data.DependencyData;
 import io.github.vshnv.slimjar.resolver.data.Mirror;
 import io.github.vshnv.slimjar.resolver.data.Repository;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,29 +17,39 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class SimpleJsonDependencyReader implements DependencyReader {
-    @Override
-    public DependencyData read(InputStream inputStream) {
-        final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        final JsonObject element = JsonParser.parseReader(inputStreamReader).getAsJsonObject();
-        final JsonArray mirrorsArray = element.getAsJsonArray("mirrors");
-        final JsonArray repositoriesArray = element.getAsJsonArray("repositories");
-        final JsonArray dependenciesArray = element.getAsJsonArray("dependencies");
-        final JsonArray relocationsArray = element.getAsJsonArray("relocations");
-        final Collection<Mirror> mirrors = parseMirrors(mirrorsArray);
-        final Collection<Repository> repositories = parseRepositoryList(repositoriesArray);
-        final Collection<Dependency> dependencies = parseDependencyList(dependenciesArray);
-        final Collection<RelocationRule> relocations = parseRelocations(relocationsArray);
-        return new DependencyData(mirrors, repositories, dependencies, relocations);
+    private final JSONParser parser;
+
+    public SimpleJsonDependencyReader(JSONParser parser) {
+        this.parser = parser;
     }
 
-    private static Collection<RelocationRule> parseRelocations(JsonArray dependenciesArray) {
+    @Override
+    public DependencyData read(InputStream inputStream) {
+        try {
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final JSONObject jsonObject = (JSONObject) parser.parse(inputStreamReader);
+            final JSONArray mirrorsArray = (JSONArray) jsonObject.get("mirrors");
+            final JSONArray repositoriesArray = (JSONArray) jsonObject.get("repositories");
+            final JSONArray dependenciesArray = (JSONArray) jsonObject.get("dependencies");
+            final JSONArray relocationsArray = (JSONArray) jsonObject.get("relocations");
+            final Collection<Mirror> mirrors = parseMirrors(mirrorsArray);
+            final Collection<Repository> repositories = parseRepositoryList(repositoriesArray);
+            final Collection<Dependency> dependencies = parseDependencyList(dependenciesArray);
+            final Collection<RelocationRule> relocations = parseRelocations(relocationsArray);
+            return new DependencyData(mirrors, repositories, dependencies, relocations);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    private static Collection<RelocationRule> parseRelocations(JSONArray dependenciesArray) {
         final Collection<RelocationRule> relocationRules = new HashSet<>();
-        for (final JsonElement element : dependenciesArray) {
-            final JsonObject elementObject = element.getAsJsonObject();
-            final String originalPattern = elementObject.get("originalPackagePattern").getAsString();
-            final String relocatedPattern = elementObject.get("relocatedPackagePattern").getAsString();
-            final JsonArray exclusionsArray = elementObject.getAsJsonArray("exclusions");
-            final JsonArray inclusionsArray = elementObject.getAsJsonArray("inclusions");
+        for (final Object elementObj : dependenciesArray) {
+            final JSONObject element = (JSONObject) elementObj;
+            final String originalPattern = (String) element.get("originalPackagePattern");
+            final String relocatedPattern = (String) element.get("relocatedPackagePattern");
+            final JSONArray exclusionsArray = (JSONArray) element.get("exclusions");
+            final JSONArray inclusionsArray = (JSONArray) element.get("inclusions");
             final Collection<String> exclusions = parseStringArray(exclusionsArray);
             final Collection<String> inclusions = parseStringArray(inclusionsArray);
             final RelocationRule relocationRule = new RelocationRule(originalPattern, relocatedPattern, exclusions, inclusions);
@@ -46,40 +58,36 @@ public class SimpleJsonDependencyReader implements DependencyReader {
         return relocationRules;
     }
 
-    private static Collection<String> parseStringArray(JsonArray array) {
+    private static Collection<String> parseStringArray(JSONArray array) {
         final Collection<String> result = new HashSet<>();
-        for (final JsonElement element : array) {
-            result.add(element.getAsString());
+        for (final Object element : array) {
+            result.add((String) element);
         }
         return result;
     }
 
-    private static Collection<Dependency> parseDependencyList(JsonArray dependenciesArray) {
+    private static Collection<Dependency> parseDependencyList(JSONArray dependenciesArray) {
         final Collection<Dependency> result = new HashSet<>();
-        for (final JsonElement element : dependenciesArray) {
-            result.add(parseDependency(element.getAsJsonObject()));
+        for (final Object element : dependenciesArray) {
+            result.add(parseDependency((JSONObject) element));
         }
         return result;
     }
 
-    private static Dependency parseDependency(JsonObject dependency) {
-        final String groupId = dependency.get("groupId").getAsString();
-        final String artifactId = dependency.get("artifactId").getAsString();
-        final String version = dependency.get("version").getAsString();
-        final JsonElement snapshotElement = dependency.get("snapshotId");
-        String snapshotId = null;
-        if (dependency.has("snapshotId")) {
-            snapshotId = dependency.get("snapshotId").getAsString();
-        }
-        final Collection<Dependency> transitive = parseDependencyList(dependency.getAsJsonArray("transitive"));
+    private static Dependency parseDependency(JSONObject dependency) {
+        final String groupId = (String) dependency.get("groupId");
+        final String artifactId = (String) dependency.get("artifactId");
+        final String version = (String) dependency.get("version");
+        final String snapshotId = (String) dependency.get("snapshotId");
+        final Collection<Dependency> transitive = parseDependencyList((JSONArray) dependency.get("transitive"));
         return new Dependency(groupId, artifactId, version, snapshotId, transitive);
     }
 
-    private static Collection<Repository> parseRepositoryList(JsonArray repositoriesArray) {
+    private static Collection<Repository> parseRepositoryList(JSONArray repositoriesArray) {
         final Collection<Repository> result = new HashSet<>();
-        for (final JsonElement element : repositoriesArray) {
+        for (final Object element : repositoriesArray) {
             try {
-                result.add(parseRepository(element.getAsJsonObject()));
+                result.add(parseRepository((JSONObject) element));
             } catch (MalformedURLException e) {
                 // Ignored malformed repository
             }
@@ -87,17 +95,17 @@ public class SimpleJsonDependencyReader implements DependencyReader {
         return result;
     }
 
-    private static Repository parseRepository(JsonObject repository) throws MalformedURLException {
-        final String urlStr = repository.get("url").getAsString();
+    private static Repository parseRepository(JSONObject repository) throws MalformedURLException {
+        final String urlStr = (String) repository.get("url");
         final URL url = new URL(urlStr);
         return new Repository(url);
     }
 
-    private static Collection<Mirror> parseMirrors(JsonArray mirrorsArray) {
+    private static Collection<Mirror> parseMirrors(JSONArray mirrorsArray) {
         final Collection<Mirror> result = new HashSet<>();
-        for (final JsonElement element : mirrorsArray) {
+        for (final Object element : mirrorsArray) {
             try {
-                result.add(parseMirror(element.getAsJsonObject()));
+                result.add(parseMirror((JSONObject)element));
             } catch (MalformedURLException e) {
                 // Ignored malformed repository
             }
@@ -105,9 +113,9 @@ public class SimpleJsonDependencyReader implements DependencyReader {
         return result;
     }
 
-    private static Mirror parseMirror(JsonObject mirror) throws MalformedURLException {
-        final String mirroringStr = mirror.get("mirroring").getAsString();
-        final String originalStr = mirror.get("original").getAsString();
+    private static Mirror parseMirror(JSONObject mirror) throws MalformedURLException {
+        final String mirroringStr = (String) mirror.get("mirroring");
+        final String originalStr = (String) mirror.get("original");
         final URL mirroring = new URL(mirroringStr);
         final URL original = new URL(originalStr);
         return new Mirror(mirroring, original);
