aa22d58cafeb83d0c38cdeda0c2248a9e6bea705
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-09 06:11:00 +0530

    Added better isolated class loader

--- slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/InjectableClassLoader.java
+++ slimjar/src/main/java/io/github/vshnv/slimjar/injector/loader/InjectableClassLoader.java
@@ -7,61 +7,19 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-public final class InjectableClassLoader extends URLClassLoader implements Injectable {
-    private final String entryPointClass;
-
-    public InjectableClassLoader(final String entryPointClass) {
-        super(new URL[0]);
-        this.entryPointClass = entryPointClass;
+public abstract class InjectableClassLoader extends URLClassLoader implements Injectable {
+    static {
+        registerAsParallelCapable();
     }
+
+    public InjectableClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
 
     @Override
     public void inject(final URL url) {
         addURL(url);
-    }
-
-    @Override
-    public Class<?> loadClass(final String name) throws ClassNotFoundException {
-        if (name.equals(entryPointClass)) {
-            final Class<?> loadedClass = findLoadedClass(entryPointClass);
-            if (loadedClass == null) return findClass(name);
-            if (loadedClass.getClassLoader() != this) {
-                throw new AssertionError("Application class already loaded by another classloader!");
-            }
-            return findClass(name);
-        }
-        return super.loadClass(name);
-    }
-
-    @Override
-    public Class<?> findClass(final String name) throws ClassNotFoundException {
-        Class<?> loaded = findLoadedClass(name);
-        if (loaded != null) return loaded;
-
-        byte[] b = loadClassFromFile(name);
-        if (b != null) return defineClass(name, b, 0, b.length);
-
-        return super.findClass(name);
-    }
-
-    private byte[] loadClassFromFile(final String fileName)  {
-        InputStream inputStream = getParent().getResourceAsStream(
-                fileName.replace('.', File.separatorChar) + ".class");
-        if (inputStream == null) {
-            return null;
-        }
-        byte[] buffer;
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        int nextValue = 0;
-        try {
-            while ( (nextValue = inputStream.read()) != -1 ) {
-                byteStream.write(nextValue);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        buffer = byteStream.toByteArray();
-        return buffer;
     }
 
 }
