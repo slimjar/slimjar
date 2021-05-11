d698dd63d31abff2f574cfe4654310680a6c6c78
Matt <xpsykogfx@gmail.com>
2021-05-11 01:31:53 +0100

    Fixed transitive issue (I think), and added compileOnly to isolated.

--- gradle-plugin/src/main/kotlin/io/github/slimjar/func/Declarations.kt
+++ gradle-plugin/src/main/kotlin/io/github/slimjar/func/Declarations.kt
@@ -13,14 +13,13 @@ val Project.slimDefaultDependency: Boolean
 /**
  * Utility for creating a configuration that extends another
  */
-internal fun Project.createConfig(configName: String, extends: String, transitive: Boolean = false): Configuration {
+internal fun Project.createConfig(configName: String, extends: String): Configuration {
     val compileOnlyConfig = configurations.findByName(extends)
         ?: throw ConfigurationNotFoundException("Could not find `$extends` configuration!")
 
     val slimConfig = configurations.create(configName)
     compileOnlyConfig.extendsFrom(slimConfig)
-    // TODO need to test this one, probably doesn't do what I think it does
-    slimConfig.isTransitive = transitive
+    slimConfig.isTransitive = true
 
     return slimConfig
 }
