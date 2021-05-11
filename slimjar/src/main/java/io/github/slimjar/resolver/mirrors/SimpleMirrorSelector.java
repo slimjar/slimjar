252cef6bc75a30a6ebe3b54165b403ea0ad7c070
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-11 12:34:03 +0530

    Fixed issue in mirror selector

--- slimjar/src/main/java/io/github/slimjar/resolver/mirrors/SimpleMirrorSelector.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/mirrors/SimpleMirrorSelector.java
@@ -6,13 +6,21 @@ import io.github.slimjar.resolver.data.Repository;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public final class SimpleMirrorSelector implements MirrorSelector {
-    private static final String DEFUALT_CENTRAL_MIRROR_URL = "https://repo.vshnv.tech/";
+    private static final String DEFAULT_CENTRAL_MIRROR_URL = "https://repo.vshnv.tech/";
     private static final String CENTRAL_URL = "https://repo.maven.apache.org/maven2/";
     private static final String ALT_CENTRAL_URL = "https://repo1.maven.org/maven2/";
+    private static final Collection<String> CENTRAL_REPO;
+
+    static {
+        final Set<String> central = new HashSet<>();
+        central.add(CENTRAL_URL);
+        central.add(ALT_CENTRAL_URL);
+        CENTRAL_REPO = Collections.unmodifiableSet(central);
+    }
 
     @Override
     public Collection<Repository> select(final Collection<Repository> mainRepositories, final Collection<Mirror> mirrors) throws MalformedURLException {
@@ -21,21 +29,21 @@ public final class SimpleMirrorSelector implements MirrorSelector {
                 .collect(Collectors.toSet());
         final Collection<Repository> resolved = mainRepositories.stream()
                 .filter(repo -> !originals.contains(repo.getUrl()))
+                .filter(repo -> !isCentral(repo))
                 .collect(Collectors.toSet());
         final Collection<Repository> mirrored = mirrors.stream()
                 .map(Mirror::getMirroring)
                 .map(Repository::new)
                 .collect(Collectors.toSet());
-        boolean isCentralMirrored = resolved.stream()
-                .map(Repository::getUrl)
-                .map(URL::toString)
-                .noneMatch(repo -> repo.equals(CENTRAL_URL) || repo.equals(ALT_CENTRAL_URL));
         resolved.addAll(mirrored);
-        if (!isCentralMirrored) {
-            resolved.add(new Repository(
-                    new URL(DEFUALT_CENTRAL_MIRROR_URL)
-            ));
-        }
+        resolved.add(new Repository(
+                new URL(DEFAULT_CENTRAL_MIRROR_URL)
+        ));
         return resolved;
+    }
+
+    private static boolean isCentral(final Repository repository) {
+        final String url = repository.getUrl().toString();
+        return CENTRAL_REPO.contains(url);
     }
 }
