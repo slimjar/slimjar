3b04e1ad525dbb5dad6eb2bfd5fe444f0cb4d9e6
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-13 14:46:31 +0530

    Dependency data provider tests

--- slimjar/src/test/java/io/github/slimjar/resolver/reader/provider/FileDependencyDataProviderTest.java
+++ slimjar/src/test/java/io/github/slimjar/resolver/reader/provider/FileDependencyDataProviderTest.java
@@ -1,29 +1,23 @@
-package io.github.slimjar.resolver.reader;
+package io.github.slimjar.resolver.reader.provider;
 
 import com.google.gson.Gson;
-import org.junit.Test;
+import io.github.slimjar.resolver.reader.*;
+import junit.framework.TestCase;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-
-import static org.junit.Assert.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({URL.class, FileDependencyDataProvider.class})
-public class DependencyDataProviderTest {
-    @Test
+public class FileDependencyDataProviderTest extends TestCase {
     public void testFileDependencyDataProvider() throws Exception {
         final MockDependencyData mockDependencyData = new MockDependencyData();
         final URL mockUrl = PowerMockito.mock(URL.class);
         PowerMockito.whenNew(URL.class).withParameterTypes(String.class).withArguments("MyURLString").thenReturn(mockUrl);
         PowerMockito.when(mockUrl.openStream()).thenReturn(mockDependencyData.getDependencyDataInputStream());
         final DependencyDataProvider dependencyDataProvider = new FileDependencyDataProvider(new GsonDependencyReader(new Gson()), mockUrl);
-        assertEquals(mockDependencyData.getExpectedSample(), dependencyDataProvider.get());
+        assertEquals("Read and provide proper dependencies",mockDependencyData.getExpectedSample(), dependencyDataProvider.get());
     }
 }
---
+++ slimjar/src/test/java/io/github/slimjar/resolver/reader/provider/ModuleDependencyDataProviderTest.java
@@ -0,0 +1,62 @@
+package io.github.slimjar.resolver.reader.provider;
+
+import com.google.gson.Gson;
+import io.github.slimjar.resolver.data.DependencyData;
+import io.github.slimjar.resolver.reader.DependencyDataProvider;
+import io.github.slimjar.resolver.reader.GsonDependencyReader;
+import io.github.slimjar.resolver.reader.MockDependencyData;
+import io.github.slimjar.resolver.reader.ModuleDependencyDataProvider;
+import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({URL.class, ModuleDependencyDataProviderTest.class, ModuleDependencyDataProvider.class})
+public class ModuleDependencyDataProviderTest extends TestCase {
+    public void testModuleDependencyDataProviderNonEmpty() throws Exception {
+        final MockDependencyData mockDependencyData = new MockDependencyData();
+        final URL mockUrl = PowerMockito.mock(URL.class);
+        final JarURLConnection jarURLConnection = PowerMockito.mock(JarURLConnection.class);
+        final JarFile jarFile = PowerMockito.mock(JarFile.class);
+        final ZipEntry zipEntry = PowerMockito.mock(ZipEntry.class);
+        final InputStream inputStream = mockDependencyData.getDependencyDataInputStream();
+        PowerMockito.whenNew(URL.class).withAnyArguments().thenReturn(mockUrl);
+        PowerMockito.when(mockUrl.openConnection()).thenReturn(jarURLConnection);
+        PowerMockito.when(jarURLConnection.getJarFile()).thenReturn(jarFile);
+        PowerMockito.when(jarFile.getEntry("slimjar.json")).thenReturn(zipEntry);
+        PowerMockito.when(jarFile.getInputStream(zipEntry)).thenReturn(inputStream);
+        final DependencyDataProvider dependencyDataProvider = new ModuleDependencyDataProvider(new GsonDependencyReader(new Gson()), mockUrl);
+        assertEquals("Read and provide proper dependencies", mockDependencyData.getExpectedSample(), dependencyDataProvider.get());
+    }
+
+    public void testModuleDependencyDataProviderEmpty() throws Exception {
+        final MockDependencyData mockDependencyData = new MockDependencyData();
+        final URL mockUrl = PowerMockito.mock(URL.class);
+        final JarURLConnection jarURLConnection = PowerMockito.mock(JarURLConnection.class);
+        final JarFile jarFile = PowerMockito.mock(JarFile.class);
+        final DependencyData emptyDependency = new DependencyData(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+        PowerMockito.whenNew(URL.class).withAnyArguments().thenReturn(mockUrl);
+        PowerMockito.when(mockUrl.openConnection()).thenReturn(jarURLConnection);
+        PowerMockito.when(jarURLConnection.getJarFile()).thenReturn(jarFile);
+        PowerMockito.when(jarFile.getEntry("slimjar.json")).thenReturn(null);
+        final DependencyDataProvider dependencyDataProvider = new ModuleDependencyDataProvider(new GsonDependencyReader(new Gson()), mockUrl);
+        assertEquals("Empty dependency if not exists", emptyDependency, dependencyDataProvider.get());
+    }
+}
