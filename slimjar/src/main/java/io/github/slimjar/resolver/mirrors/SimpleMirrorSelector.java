75a567a52136df930988dcdd4a477832d143ee8b
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-13 15:19:04 +0530

    Made mirror selector constants public for tests

--- slimjar/src/main/java/io/github/slimjar/resolver/mirrors/SimpleMirrorSelector.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/mirrors/SimpleMirrorSelector.java
@@ -34,9 +34,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public final class SimpleMirrorSelector implements MirrorSelector {
-    private static final String DEFAULT_CENTRAL_MIRROR_URL = "https://repo.vshnv.tech/";
-    private static final String CENTRAL_URL = "https://repo.maven.apache.org/maven2/";
-    private static final String ALT_CENTRAL_URL = "https://repo1.maven.org/maven2/";
+    public static final String DEFAULT_CENTRAL_MIRROR_URL = "https://repo.vshnv.tech/";
+    public static final String CENTRAL_URL = "https://repo.maven.apache.org/maven2/";
+    public static final String ALT_CENTRAL_URL = "https://repo1.maven.org/maven2/";
     private static final Collection<String> CENTRAL_REPO;
 
     static {
