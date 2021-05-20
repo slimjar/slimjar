2f13a30aace8a0268e6ac5db15c160e58a3b86ec
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-20 17:12:52 +0530

    Added PassthroughRelocator

---
+++ slimjar/src/main/java/io/github/slimjar/relocation/PassthroughRelocator.java
@@ -0,0 +1,11 @@
+package io.github.slimjar.relocation;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class PassthroughRelocator implements Relocator {
+    @Override
+    public void relocate(File input, File output) throws IOException {
+
+    }
+}
