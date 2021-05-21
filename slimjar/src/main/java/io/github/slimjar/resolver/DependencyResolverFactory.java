38f532e3b492d8efd35f49015f83f62aedd0888a
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-21 19:28:47 +0530

    Added functional interface annotations

--- slimjar/src/main/java/io/github/slimjar/resolver/DependencyResolverFactory.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/DependencyResolverFactory.java
@@ -5,6 +5,7 @@ import io.github.slimjar.resolver.enquirer.RepositoryEnquirerFactory;
 
 import java.util.Collection;
 
+@FunctionalInterface
 public interface DependencyResolverFactory {
     DependencyResolver create(final Collection<Repository> repositories, final RepositoryEnquirerFactory enquirerFactory);
 }
