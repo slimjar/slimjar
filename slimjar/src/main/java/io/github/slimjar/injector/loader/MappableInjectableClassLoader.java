7c4f2606a51dccac60b91dfdf500a2fca52232a7
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-22 23:07:44 +0530

    Switched to inheriting InjectableClassLoader

--- slimjar/src/main/java/io/github/slimjar/injector/loader/MappableInjectableClassLoader.java
+++ slimjar/src/main/java/io/github/slimjar/injector/loader/MappableInjectableClassLoader.java
@@ -1,15 +1,14 @@
 package io.github.slimjar.injector.loader;
 
 import java.net.URL;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class MappableInjectableClassLoader extends IsolatedInjectableClassLoader {
+public final class MappableInjectableClassLoader extends InjectableClassLoader {
     private final Map<String, Class<?>> mappings;
 
-    public MappableInjectableClassLoader(final URL[] urls, final Collection<Class<?>> delegates, final Map<String, Class<?>> mappings) {
-        super(urls, delegates);
+    public MappableInjectableClassLoader(final URL[] urls, final Map<String, Class<?>> mappings) {
+        super(urls, getSystemClassLoader().getParent());
         this.mappings = new HashMap<>(mappings);
     }
 
