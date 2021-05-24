7ef01c5ea34daf7e4979ebc9bd79803eb9c8a57d
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-25 03:52:23 +0530

    Added throws - ModuleDependencyDataProvider - ReflectiveOperationException

--- slimjar/src/main/java/io/github/slimjar/resolver/reader/ModuleDependencyDataProvider.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/reader/ModuleDependencyDataProvider.java
@@ -48,8 +48,9 @@ public final class ModuleDependencyDataProvider implements DependencyDataProvide
     }
 
     @Override
-    public DependencyData get() throws IOException {
+    public DependencyData get() throws IOException, ReflectiveOperationException {
         final URL depFileURL = new URL("jar:file:" +moduleUrl.getFile() +"!/slimjar.json");
+
         final URLConnection connection = depFileURL.openConnection();
         if (!(connection instanceof JarURLConnection)) {
             throw new AssertionError("Invalid Module URL provided(Non-Jar File)");
@@ -65,6 +66,7 @@ public final class ModuleDependencyDataProvider implements DependencyDataProvide
                     Collections.emptySet()
             );
         }
+
         try (InputStream inputStream = jarFile.getInputStream(dependencyFileEntry)){
             return dependencyReader.read(inputStream);
         }
