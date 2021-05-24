ca25782942058044dc19a71bebce5f6a5a418327
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-25 03:35:32 +0530

    Modified app factory for compat

--- slimjar/src/main/java/io/github/slimjar/app/ApplicationFactory.java
+++ slimjar/src/main/java/io/github/slimjar/app/ApplicationFactory.java
@@ -24,80 +24,27 @@
 
 package io.github.slimjar.app;
 
-import io.github.slimjar.app.external.DependencyProvider;
-import io.github.slimjar.app.external.DependencyProviderFactory;
-import io.github.slimjar.app.module.TemporaryModuleExtractor;
-import io.github.slimjar.injector.loader.IsolatedInjectableClassLoader;
-import io.github.slimjar.resolver.data.DependencyData;
-import io.github.slimjar.app.module.ModuleExtractor;
-import io.github.slimjar.injector.DependencyInjector;
-import io.github.slimjar.injector.loader.Injectable;
-import io.github.slimjar.injector.loader.InjectableClassLoader;
-import io.github.slimjar.injector.loader.WrappedInjectableClassLoader;
-import io.github.slimjar.resolver.reader.DependencyDataProvider;
-import io.github.slimjar.resolver.reader.DependencyDataProviderFactory;
-import io.github.slimjar.util.Parameters;
+import io.github.slimjar.app.builder.ApplicationBuilder;
+import io.github.slimjar.app.builder.IsolationConfiguration;
 import java.io.IOException;
-import java.net.URL;
+import java.net.URISyntaxException;
 import java.net.URLClassLoader;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 
 @Deprecated
 public final class ApplicationFactory {
-    private final ApplicationConfiguration applicationConfiguration;
-
-    public ApplicationFactory(final ApplicationConfiguration applicationConfiguration) {
-        this.applicationConfiguration = applicationConfiguration;
-    }
-
-    public ApplicationConfiguration getApplicationConfiguration() {
-        return applicationConfiguration;
-    }
-
-    public Application createIsolatedApplication(final Collection<String> moduleNames, final String fqClassName, final Object... args) throws ReflectiveOperationException, ClassCastException, IOException {
+    public Application createIsolatedApplication(final Collection<String> moduleNames, final String fqClassName, final Object... args) throws ClassCastException {
         return createIsolatedApplication(moduleNames, fqClassName, ClassLoader.getSystemClassLoader().getParent(), args);
     }
 
-    public Application createIsolatedApplication(final Collection<String> moduleNames, final String fqClassName, final ClassLoader parent, final Object... args) throws ReflectiveOperationException, ClassCastException, IOException {
-        final DependencyProvider dependencyProvider = DependencyProviderFactory.createExternalDependencyProvider();
-        final ModuleExtractor moduleExtractor = new TemporaryModuleExtractor();
-        final URL[] extractedModules = findExtractedModules(moduleExtractor, moduleNames);
-        final DependencyDataProviderFactory dependencyDataProviderFactory = dependencyProvider.createDependencyDataProviderFactory();
-        final InjectableClassLoader classLoader = new IsolatedInjectableClassLoader(extractedModules, parent, Collections.singleton(Application.class));
-        final DependencyInjector dependencyInjector = applicationConfiguration.getDependencyInjector();
-
-        for (final URL module : extractedModules) {
-            final DependencyDataProvider dataProvider = dependencyDataProviderFactory.forModule(module);
-            final DependencyData dependencyData = dataProvider.get();
-            dependencyInjector.inject(classLoader, dependencyData.getDependencies());
-        }
-        final Class<Application> applicationClass = (Class<Application>) Class.forName(fqClassName, true, classLoader);
-        return applicationClass.getConstructor(Parameters.typesFrom(args)).newInstance(args);
+    public Application createIsolatedApplication(final String applicationName, final Collection<String> moduleNames, final String fqClassName, final ClassLoader parent, final Object... args) throws ReflectiveOperationException, ClassCastException, IOException, URISyntaxException, NoSuchAlgorithmException {
+        final IsolationConfiguration config = new IsolationConfiguration.Builder()
+                .modules(moduleNames).applicationClass(fqClassName).parentClassLoader(parent).build();
+        return ApplicationBuilder.isolated(applicationName, config, args).build();
     }
 
-    public Application createAppendingApplication(final URLClassLoader classLoader) {
-        final DependencyData dependencyData = applicationConfiguration.getDependencyData();
-        final DependencyInjector dependencyInjector = applicationConfiguration.getDependencyInjector();
-        final Injectable injectable = new WrappedInjectableClassLoader(classLoader);
-        dependencyInjector.inject(injectable, dependencyData.getDependencies());
-        return new AppendingApplication();
+    public Application createAppendingApplication(final String applicationName, final URLClassLoader classLoader) throws URISyntaxException, ReflectiveOperationException, NoSuchAlgorithmException, IOException {
+        return ApplicationBuilder.appending(applicationName, classLoader).build();
     }
-
-    private static URL findModule(final String moduleName) {
-        final ClassLoader classLoader = ApplicationFactory.class.getClassLoader();
-        return classLoader.getResource(moduleName + ".isolated-jar");
-    }
-
-    private static URL[] findExtractedModules(final ModuleExtractor extractor, final Collection<String> moduleNames) throws IOException {
-        final Collection<URL> result = new HashSet<>();
-        for (final String moduleName : moduleNames) {
-            final URL modulePath = findModule(moduleName);
-            final URL extractedModule = extractor.extractModule(modulePath, moduleName);
-            result.add(extractedModule);
-        }
-        return result.stream().toArray(URL[]::new);
-    }
-
 }
