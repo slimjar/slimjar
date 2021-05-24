d7f881dc6ff265b86e67d6efdce0cb0e6c21870e
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-25 03:38:56 +0530

    Removed unused external-loader classes

--- slimjar/src/main/java/io/github/slimjar/app/external/ExposedClassHelper.java
+++
@@ -1,9 +0,0 @@
-package io.github.slimjar.app.external;
-
-import java.util.Collection;
-import java.util.Map;
-
-public interface ExposedClassHelper {
-    Collection<Class<?>> fetchExposedClasses();
-    Map<String, Class<?>> fetchRemappedRelocations();
-}
