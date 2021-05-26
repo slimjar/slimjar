eaf72eac6bf3803654243858ad3b3c53f4639a67
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-26 21:43:17 +0530

    Refactored Downloader factory to use Verifier

--- slimjar/src/main/java/io/github/slimjar/downloader/DependencyDownloaderFactory.java
+++ slimjar/src/main/java/io/github/slimjar/downloader/DependencyDownloaderFactory.java
@@ -1,9 +1,10 @@
 package io.github.slimjar.downloader;
 
 import io.github.slimjar.downloader.output.OutputWriterFactory;
+import io.github.slimjar.downloader.verify.DependencyVerifier;
 import io.github.slimjar.resolver.DependencyResolver;
 
 @FunctionalInterface
 public interface DependencyDownloaderFactory {
-    DependencyDownloader create(final OutputWriterFactory outputWriterFactory, final DependencyResolver resolver);
+    DependencyDownloader create(final OutputWriterFactory outputWriterFactory, final DependencyResolver resolver, final DependencyVerifier verifier);
 }
