54486d73291e29225d95c95bb8ae4e17b302be09
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-05-27 16:53:57 +0530

    Refactored InstrumentationInjectable to use InstrumentationFactory

--- slimjar/src/main/java/io/github/slimjar/injector/loader/InstrumentationInjectable.java
+++ slimjar/src/main/java/io/github/slimjar/injector/loader/InstrumentationInjectable.java
@@ -3,12 +3,15 @@ package io.github.slimjar.injector.loader;
 import io.github.slimjar.app.builder.ApplicationBuilder;
 import io.github.slimjar.app.module.ModuleExtractor;
 import io.github.slimjar.app.module.TemporaryModuleExtractor;
+import io.github.slimjar.injector.agent.ByteBuddyInstrumentationFactory;
+import io.github.slimjar.injector.agent.InstrumentationFactory;
+import io.github.slimjar.injector.loader.manifest.JarManifestGenerator;
+import io.github.slimjar.injector.loader.manifest.ManifestGenerator;
 import io.github.slimjar.relocation.JarFileRelocator;
 import io.github.slimjar.relocation.PassthroughRelocator;
 import io.github.slimjar.relocation.RelocationRule;
 import io.github.slimjar.relocation.Relocator;
 import io.github.slimjar.relocation.facade.JarRelocatorFacadeFactory;
-import io.github.slimjar.relocation.facade.ReflectiveJarRelocatorFacade;
 import io.github.slimjar.relocation.facade.ReflectiveJarRelocatorFacadeFactory;
 import io.github.slimjar.resolver.data.Dependency;
 import io.github.slimjar.resolver.data.DependencyData;
@@ -29,10 +32,6 @@ import java.util.UUID;
 import java.util.jar.JarFile;
 
 public final class InstrumentationInjectable implements Injectable {
-    private static final String AGENT_JAR = "loader-agent.isolated-jar";
-    private static final String AGENT_PACKAGE = "io#github#slimjar#injector#agent";
-    private static final String AGENT_CLASS = "ClassLoaderAgent";
-    private static final String BYTEBUDDY_AGENT_CLASS = "net#bytebuddy#agent#ByteBuddyAgent";
 
 
 
@@ -48,45 +47,14 @@ public final class InstrumentationInjectable implements Injectable {
     }
 
     public static Injectable create() throws IOException, NoSuchAlgorithmException, ReflectiveOperationException, URISyntaxException {
-        final URL url = InstrumentationInjectable.class.getClassLoader().getResource(AGENT_JAR);
-        final ModuleExtractor extractor = new TemporaryModuleExtractor();
-        final URL extractedURL = extractor.extractModule(url, "loader-agent");
-
-        final JarRelocatorFacadeFactory facadeFactory = ReflectiveJarRelocatorFacadeFactory.create();
-        final String pattern = generatePattern();
-        final String relocatedAgentClass = String.format("%s.%s", pattern, AGENT_CLASS);
-        final RelocationRule relocationRule = new RelocationRule(Packages.fix(AGENT_PACKAGE), pattern, Collections.emptySet(), Collections.emptySet());
-        final Relocator relocator = new JarFileRelocator(Collections.singleton(relocationRule), facadeFactory);
-        final File inputFile = new File(extractedURL.getPath());
-        final File relocatedFile = File.createTempFile("slimjar-agent", ".jar");
-        relocator.relocate(inputFile, relocatedFile);
-
-        final InjectableClassLoader classLoader = new IsolatedInjectableClassLoader();
-
-        ApplicationBuilder.injecting("SlimJar-Agent", classLoader)
-                .dataProviderFactory((dataUrl) -> InstrumentationInjectable::getDependency)
-                .relocatorFactory((rules) -> new PassthroughRelocator())
-                .relocationHelperFactory((rel) -> (dependency,file) -> file)
-                .build();
-        final Class<?> byteBuddyAgentClass = Class.forName(Packages.fix(BYTEBUDDY_AGENT_CLASS), true, classLoader);
-        final Method attachMethod = byteBuddyAgentClass.getMethod("attach", File.class, String.class, String.class);
-
-        final Class<?> processHandle = Class.forName("java.lang.ProcessHandle");
-        final Method currentMethod = processHandle.getMethod("current");
-        final Method pidMethod = processHandle.getMethod("pid");
-        final Object currentProcess = currentMethod.invoke(processHandle);
-        final Long processId = (Long) pidMethod.invoke(currentProcess);
-
-
-        attachMethod.invoke(null, relocatedFile, String.valueOf(processId), "");
-
-        final Class<?> agentClass = Class.forName(relocatedAgentClass, true, ClassLoader.getSystemClassLoader());
-        final Method instrMethod = agentClass.getMethod("getInstrumentation");
-        final Instrumentation instrumentation = (Instrumentation) instrMethod.invoke(null);
-        return new InstrumentationInjectable(instrumentation);
+        return create(new ByteBuddyInstrumentationFactory());
     }
 
-    private static DependencyData getDependency() throws MalformedURLException {
+    public static Injectable create(final InstrumentationFactory factory) throws IOException, NoSuchAlgorithmException, ReflectiveOperationException, URISyntaxException {
+        return new InstrumentationInjectable(factory.create());
+    }
+
+    public static DependencyData getDependency() throws MalformedURLException {
         final Dependency byteBuddy = new Dependency(
                 "net.bytebuddy",
                 "byte-buddy-agent",
