24dff7394a7dbd43ab3a49e50f507c49b4b88c69
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-17 03:38:32 +0530

    Switched to accepting a hash in VerifyingRelocationHelper

--- slimjar/src/main/java/io/github/slimjar/relocation/helper/VerifyingRelocationHelper.java
+++ slimjar/src/main/java/io/github/slimjar/relocation/helper/VerifyingRelocationHelper.java
@@ -25,6 +25,7 @@
 package io.github.slimjar.relocation.helper;
 
 import io.github.slimjar.downloader.strategy.FilePathStrategy;
+import io.github.slimjar.downloader.verify.FileChecksumCalculator;
 import io.github.slimjar.relocation.Relocator;
 import io.github.slimjar.relocation.meta.MetaMediator;
 import io.github.slimjar.relocation.meta.MetaMediatorFactory;
@@ -34,14 +35,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public final class VerifyingRelocationHelper implements RelocationHelper {
@@ -50,23 +44,21 @@ public final class VerifyingRelocationHelper implements RelocationHelper {
     private final String selfHash;
     private final MetaMediatorFactory mediatorFactory;
 
-    public VerifyingRelocationHelper(final FilePathStrategy outputFilePathStrategy, final Relocator relocator, final MetaMediatorFactory mediatorFactory) throws URISyntaxException, NoSuchAlgorithmException, IOException {
+    public VerifyingRelocationHelper(final String selfHash, final FilePathStrategy outputFilePathStrategy, final Relocator relocator, final MetaMediatorFactory mediatorFactory) throws URISyntaxException, NoSuchAlgorithmException, IOException {
         this.mediatorFactory = mediatorFactory;
-        final URL jarURL = getClass().getProtectionDomain().getCodeSource().getLocation();
-
         this.outputFilePathStrategy = outputFilePathStrategy;
         this.relocator = relocator;
-        this.selfHash = Charset.defaultCharset().decode(ByteBuffer.wrap(findHash(jarURL))).toString();
+        this.selfHash = selfHash;
     }
 
     @Override
-    public File relocate(Dependency dependency, File file) throws IOException, ReflectiveOperationException {
+    public File relocate(final Dependency dependency, final File file) throws IOException, ReflectiveOperationException {
         final File relocatedFile = outputFilePathStrategy.selectFileFor(dependency);
         final MetaMediator metaMediator = mediatorFactory.create(relocatedFile.toPath());
         if (relocatedFile.exists()) {
             try {
                 final String ownerHash = metaMediator.readAttribute("slimjar.owner");
-                if (selfHash.equals(ownerHash)) {
+                if (selfHash.trim().equals(ownerHash.trim())) {
                     return relocatedFile;
                 }
             } catch (final FileNotFoundException | NoSuchFileException exception) {
@@ -79,11 +71,5 @@ public final class VerifyingRelocationHelper implements RelocationHelper {
         relocator.relocate(file, relocatedFile);
         metaMediator.writeAttribute("slimjar.owner", selfHash);
         return relocatedFile;
-    }
-
-    private byte[] findHash(final URL url) throws NoSuchAlgorithmException, URISyntaxException, IOException {
-        final Path jarPath = Paths.get(url.toURI());
-        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        return digest.digest(Files.readAllBytes(jarPath));
     }
 }
