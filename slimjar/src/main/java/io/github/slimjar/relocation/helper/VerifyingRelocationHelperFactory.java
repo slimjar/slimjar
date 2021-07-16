4641afd5d2fb11f44ba79f29074df1371af67d55
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-17 03:38:48 +0530

    Added hash calculating constructor

--- slimjar/src/main/java/io/github/slimjar/relocation/helper/VerifyingRelocationHelperFactory.java
+++ slimjar/src/main/java/io/github/slimjar/relocation/helper/VerifyingRelocationHelperFactory.java
@@ -25,24 +25,36 @@
 package io.github.slimjar.relocation.helper;
 
 import io.github.slimjar.downloader.strategy.FilePathStrategy;
+import io.github.slimjar.downloader.verify.FileChecksumCalculator;
 import io.github.slimjar.relocation.Relocator;
 import io.github.slimjar.relocation.meta.MetaMediatorFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 
 public class VerifyingRelocationHelperFactory implements RelocationHelperFactory {
+    private static final URL JAR_URL = VerifyingRelocationHelperFactory.class.getProtectionDomain().getCodeSource().getLocation();
+
     private final FilePathStrategy relocationFilePathStrategy;
     private final MetaMediatorFactory mediatorFactory;
+    private final String selfHash;
 
-    public VerifyingRelocationHelperFactory(final FilePathStrategy relocationFilePathStrategy, final MetaMediatorFactory mediatorFactory) {
+    public VerifyingRelocationHelperFactory(final String selfHash, final FilePathStrategy relocationFilePathStrategy, final MetaMediatorFactory mediatorFactory) {
         this.relocationFilePathStrategy = relocationFilePathStrategy;
         this.mediatorFactory = mediatorFactory;
+        this.selfHash = selfHash;
+    }
+
+    public VerifyingRelocationHelperFactory(final FileChecksumCalculator calculator, final FilePathStrategy relocationFilePathStrategy, final MetaMediatorFactory mediatorFactory) throws URISyntaxException, IOException {
+        this(calculator.calculate(new File(JAR_URL.toURI())), relocationFilePathStrategy, mediatorFactory);
     }
 
     @Override
     public RelocationHelper create(final Relocator relocator) throws URISyntaxException, IOException, NoSuchAlgorithmException {
-        return new VerifyingRelocationHelper(relocationFilePathStrategy, relocator, mediatorFactory);
+        return new VerifyingRelocationHelper(selfHash, relocationFilePathStrategy, relocator, mediatorFactory);
     }
 }
