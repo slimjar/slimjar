fd3cd7cdf6aefe5ee014b54ff0e4c316efd0a7dc
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-23 09:05:25 +0530

    Added fromJson with Type

--- slimjar/src/main/java/io/github/slimjar/resolver/reader/facade/GsonFacade.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/reader/facade/GsonFacade.java
@@ -26,7 +26,9 @@ package io.github.slimjar.resolver.reader.facade;
 
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 
 public interface GsonFacade {
     <T> T fromJson(final InputStreamReader reader, final Class<T> clazz) throws ReflectiveOperationException;
+    <T> T fromJson(final InputStreamReader reader, final Type rawType) throws ReflectiveOperationException;
 }
