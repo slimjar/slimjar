18cdafa6642d36659dc9b8fddc44340ac9f8c3d8
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-23 11:22:25 +0530

    Modified CachingDependencyResolver to use preresolved results

--- slimjar/src/main/java/io/github/slimjar/resolver/CachingDependencyResolver.java
+++ slimjar/src/main/java/io/github/slimjar/resolver/CachingDependencyResolver.java
@@ -31,6 +31,7 @@ import io.github.slimjar.resolver.data.Repository;
 import io.github.slimjar.resolver.data.Dependency;
 import io.github.slimjar.resolver.enquirer.RepositoryEnquirer;
 import io.github.slimjar.resolver.enquirer.RepositoryEnquirerFactory;
+import io.github.slimjar.resolver.pinger.URLPinger;
 
 import java.net.URL;
 import java.util.*;
@@ -40,10 +41,14 @@ import java.util.stream.Collectors;
 public final class CachingDependencyResolver implements DependencyResolver {
     private static final String FAILED_RESOLUTION_MESSAGE = "[FAILED TO RESOLVE]";
     private static final ProcessLogger LOGGER = LogDispatcher.getMediatingLogger();
+    private final URLPinger urlPinger;
     private final Collection<RepositoryEnquirer> repositories;
     private final Map<Dependency, ResolutionResult> cachedResults = new ConcurrentHashMap<>();
+    private final Map<String, ResolutionResult> preResolvedResults;
 
-    public CachingDependencyResolver(final Collection<Repository> repositories, final RepositoryEnquirerFactory enquirerFactory) {
+    public CachingDependencyResolver(final URLPinger urlPinger, final Collection<Repository> repositories, final RepositoryEnquirerFactory enquirerFactory, final Map<String, ResolutionResult> preResolvedResults) {
+        this.urlPinger = urlPinger;
+        this.preResolvedResults = new ConcurrentHashMap<>(preResolvedResults);
         this.repositories = repositories.stream()
                 .map(enquirerFactory::create)
                 .collect(Collectors.toSet());
@@ -55,6 +60,20 @@ public final class CachingDependencyResolver implements DependencyResolver {
     }
 
     private ResolutionResult attemptResolve(final Dependency dependency) {
+        final ResolutionResult preResolvedResult = preResolvedResults.get(dependency.toString());
+        if (preResolvedResult != null) {
+            if (preResolvedResult.isAggregator()) {
+                return preResolvedResult;
+            }
+            final boolean isDependencyURLValid = urlPinger.ping(preResolvedResult.getDependencyURL());
+            final URL checksumURL = preResolvedResult.getChecksumURL();
+            final boolean isChecksumURLValid = checksumURL == null || urlPinger.ping(checksumURL);
+            if (isDependencyURLValid && isChecksumURLValid) {
+                return preResolvedResult;
+            }
+        }
+
+
         final Optional<ResolutionResult> result = repositories.stream().parallel()
                 .map(enquirer -> enquirer.enquire(dependency))
                 .filter(Objects::nonNull)
