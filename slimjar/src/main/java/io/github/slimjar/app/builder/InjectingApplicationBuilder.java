22ade4f2bcec16eef2102bae849af0d7e843d3c3
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-23 11:27:26 +0530

    Added proper handling of pre resolved data

--- slimjar/src/main/java/io/github/slimjar/app/builder/InjectingApplicationBuilder.java
+++ slimjar/src/main/java/io/github/slimjar/app/builder/InjectingApplicationBuilder.java
@@ -28,11 +28,15 @@ import io.github.slimjar.app.AppendingApplication;
 import io.github.slimjar.app.Application;
 import io.github.slimjar.injector.DependencyInjector;
 import io.github.slimjar.injector.loader.*;
+import io.github.slimjar.resolver.ResolutionResult;
 import io.github.slimjar.resolver.data.DependencyData;
 import io.github.slimjar.resolver.reader.dependency.DependencyDataProvider;
+import io.github.slimjar.resolver.reader.resolution.PreResolutionDataProvider;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 import java.util.function.Function;
 
 public final class InjectingApplicationBuilder extends ApplicationBuilder {
@@ -52,7 +56,11 @@ public final class InjectingApplicationBuilder extends ApplicationBuilder {
         final DependencyDataProvider dataProvider = getDataProviderFactory().create(getDependencyFileUrl());
         final DependencyData dependencyData = dataProvider.get();
         final DependencyInjector dependencyInjector = createInjector();
-        dependencyInjector.inject(injectableSupplier.apply(this), dependencyData);
+
+        final PreResolutionDataProvider preResolutionDataProvider = getPreResolutionDataProviderFactory().create(getPreResolutionFileUrl());
+        final Map<String, ResolutionResult> preResolutionResultMap = preResolutionDataProvider.get();
+
+        dependencyInjector.inject(injectableSupplier.apply(this), dependencyData, preResolutionResultMap);
         return new AppendingApplication();
     }
 
--- slimjar/src/main/java/io/github/slimjar/app/builder/IsolatedApplicationBuilder.java
+++ slimjar/src/main/java/io/github/slimjar/app/builder/IsolatedApplicationBuilder.java
@@ -28,8 +28,10 @@ import io.github.slimjar.app.Application;
 import io.github.slimjar.injector.DependencyInjector;
 import io.github.slimjar.injector.loader.InjectableClassLoader;
 import io.github.slimjar.injector.loader.IsolatedInjectableClassLoader;
+import io.github.slimjar.resolver.ResolutionResult;
 import io.github.slimjar.resolver.data.DependencyData;
 import io.github.slimjar.resolver.reader.dependency.DependencyDataProvider;
+import io.github.slimjar.resolver.reader.resolution.PreResolutionDataProvider;
 import io.github.slimjar.util.Modules;
 import io.github.slimjar.util.Parameters;
 
@@ -38,6 +40,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.Map;
 
 public final class IsolatedApplicationBuilder extends ApplicationBuilder {
     private final IsolationConfiguration isolationConfiguration;
@@ -58,12 +61,17 @@ public final class IsolatedApplicationBuilder extends ApplicationBuilder {
 
         final DependencyDataProvider dataProvider = getDataProviderFactory().create(getDependencyFileUrl());
         final DependencyData selfDependencyData = dataProvider.get();
-        injector.inject(classLoader, selfDependencyData);
+
+        final PreResolutionDataProvider preResolutionDataProvider = getPreResolutionDataProviderFactory().create(getPreResolutionFileUrl());
+        final Map<String, ResolutionResult> preResolutionResultMap = preResolutionDataProvider.get();
+
+        injector.inject(classLoader, selfDependencyData, preResolutionResultMap);
 
         for (final URL module : moduleUrls) {
             final DependencyDataProvider moduleDataProvider = getModuleDataProviderFactory().create(module);
             final DependencyData dependencyData = moduleDataProvider.get();
-            injector.inject(classLoader, dependencyData);
+            // TODO:: fetch isolated pre-resolutions
+            injector.inject(classLoader, dependencyData, preResolutionResultMap);
         }
 
         final Class<Application> applicationClass = (Class<Application>) Class.forName(isolationConfiguration.getApplicationClass(), true, classLoader);
